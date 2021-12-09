725a3d3edc860e7d7e0b828d0729818afde79b56
on-stu <minsu0523@naver.com>
2021-12-09 13:53:32 +0900

    using hooks

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,17 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
 function Home() {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
   return "hi";
 }
 
